0964cfbac7ec44bd19de06c7e7546a2399de951e
pabloabellan03 <pabloa.0876@gmail.com>
2025-01-29 09:06:22 +0100

    primer commit de pablo

--- Hackeroo/resources/views/home.blade.php
+++ Hackeroo/resources/views/home.blade.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Document</title>
   <link
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-  />
+    rel="stylesheet" />
   <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
+
 <body>
 
   <nav class="navbar navbar-expand-sm justify-content-end">
@@ -28,7 +29,8 @@
 
   <div class="contenedor container-fluid">
     <div class="row">
-      <div class="logo col-12 d-flex justify-content-center align-items-center">        <h1 class="texto-animado">
+      <div class="logo col-12 d-flex justify-content-center align-items-center">
+        <h1 class="texto-animado">
           <span>&lt;</span>
           <span class="letra">H</span>
           <span class="letra">a</span>
@@ -43,10 +45,18 @@
         </h1>
       </div>
     </div>
+    <div class="row mt-4">
+  <div class="col-12 d-flex justify-content-center gap-4">
+    <a href="#" class="btn-app">Registrarse</a>
+    <a href="#" class="btn-app">Iniciar sesión</a>
+  </div>
+</div>
+
+
     <div class="row">
       <div class="boton col-12 d-flex justify-content-center">
         <a href="#">
-        <p>Entrar</p>
+          <p>Entrar</p>
         </a>
       </div>
     </div>
@@ -67,6 +77,7 @@
         <a href="#" class="d-block mb-2">Política de privacidad</a>
         <a href="#" class="d-block mb-2">Términos y condiciones</a>
       </div>
-    </footer>
+  </footer>
 </body>
+
 </html>
