529c37f65288596d86e54990b2d10ac2560ec701
Lorena <lo.sanchis@gmail.com>
2025-01-30 21:55:58 +0100

    pagina info OK

--- Hackeroo/resources/views/faq.blade.php
+++ Hackeroo/resources/views/faq.blade.php
@@ -2,12 +2,7 @@
 <html lang="es">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-        rel="stylesheet" />
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Plantilla Básica</title>
@@ -17,6 +12,7 @@
 
     <!-- Latest compiled JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <script src="{{ asset('js/animaciones.js') }}"></script>
