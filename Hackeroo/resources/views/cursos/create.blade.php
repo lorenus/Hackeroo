37db4f578ae7428fab01a59ebdf39d3fa69575c0
pabloabellan03 <pabloa.0876@gmail.com>
2025-01-30 20:55:09 +0100

    crear cursos acabado

--- Hackeroo/resources/views/cursos/create.blade.php
+++ Hackeroo/resources/views/cursos/create.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
 @endif
 @section('content')
 <div class="container">
