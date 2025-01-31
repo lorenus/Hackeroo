07cad49ac2b3f7b418f5a26593dc46a1965fd027
lorena <lo.sanchis@gmail.com>
2025-01-31 13:41:36 +0100

    personalizacion form

--- Hackeroo/resources/views/auth/login.blade.php
+++ Hackeroo/resources/views/auth/login.blade.php
@@ -5,45 +5,33 @@
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}">
-        @csrf
+    @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Usuario')" />
-            <br><x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    <div class="mb-3 text-center">  
+        <label for="email" class="form-label custom-label">{{ __('Usuario') }}</label>  
+        <input id="email" type="email" name="email" class="form-control custom-input border-0 border-bottom bg-transparent" value="{{ old('email') }}" required autofocus autocomplete="username">  <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Contraseña')" />
-            <br>
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+    <div class="mb-3  text-center">  <label for="password" class="form-label text-center">{{ __('Contraseña') }}</label>  
+    <input id="password" type="password" name="password" class="form-control custom-form-control border-0 border-bottom bg-transparent" required autocomplete="current-password">  <x-input-error :messages="$errors->get('password')" class="mt-2" />
+    </div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+    <div class="mb-3 form-check  text-center">  
+        <input id="remember_me" type="checkbox" class="form-check-input " name="remember">
+        <label class="form-check-label" for="remember_me">
+            {{ __('Remember me') }}
+        </label>
+    </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+    <div class="mb-3 d-flex ">  
+        @if (Route::has('password.request'))
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 me-3" href="{{ route('password.request') }}">  {{ __('Forgot your password?') }}
+            </a>
+        @endif
+    </div>
+    <div class="mt-3 text-center">
+        <x-primary-button class="ms-3  text-center">  {{ __('Log in') }}
+        </x-primary-button>
+    </div>
+</form>
 @endsection
