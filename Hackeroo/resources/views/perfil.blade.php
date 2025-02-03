223e62198846123293a6a7ba320dd6118f337fd8
Lorena <lo.sanchis@gmail.com>
2025-02-03 20:47:14 +0100

    enlaces OK

--- Hackeroo/resources/views/perfil.blade.php
+++ Hackeroo/resources/views/perfil.blade.php
@@ -13,59 +13,58 @@
 <h1>img-Perfil del Profesor</h1>
 <p>Aquí va el contenido específico para profesores...</p>
 @elseif(Auth::user()->rol == 'alumno')
-<div class="container contenedor-perfil">
+<div class="container contenedor-perfil d-flex flex-column">
     <!-- Primera fila -->
-    <div class="row align-items-center mb-4">
-        <div class="perfil row-12 d-flex align-items-center">
-            <div class="img-container m-3">
-                <img src="/img/avatares/1.png" alt="Imagen 1" class="img-fluid img-perfil">
-            </div>
-            <div>
-                <h2>Hola, <span id="nombreUsuario">Usuario</span></h2>
-            </div>
-            <div class="col-md-6 text-end">
-                <div class="img-container m-3">
-                    <img src="/img/iconos/corona.png" alt="Imagen 2" class="img-fluid img-perfil">
-                </div>
-            </div>
+        <div class="perfil row-12 mb-4 d-flex justify-content-between align-items-center ps-5 pe-5">
+            <img src="/img/avatares/1.png" alt="Imagen 1" class="img-fluid img-perfil-cabecera">
+            <h2>Hola, <span id="nombreUsuario">Usuario</span></h2>
+            <div class="puntuacion"><img src="/img/iconos/corona.png" alt="Imagen 2" class="img-fluid img-perfil"></div>
         </div>
-    </div>
+
 
     <!-- Segunda fila (enlaces) -->
-    <div class="row mt-3 mb-4">
-        <!-- Enlace 1 -->
-        <div class="div-enlace col-md-6 link-box">
-            <a href="#">
-                <img src="/img/iconos/cursos.png" alt="Enlace 1" class="img-fluid img-perfil">
-                <p>Mis cursos</p>
-            </a>
+    <div class="row d-flex justify-content-between mb-4 text-center">
+        <div class="col-12 col-md-6">
+            <div class="div-enlace">
+                <a href="#">
+                    <img src="/img/iconos/cursos.png" alt="Enlace 1" class="img-fluid img-perfil">
+                    <h4>Mis cursos</h4>
+                </a>
+            </div>
         </div>
-        <!-- Enlace 2 -->
-        <div class="div-enlace col-md-6 link-box">
-            <a href="#">
-                <img src="/img/iconos/trofeo.png" alt="Enlace 2" class="img-fluid img-perfil">
-                <p>Ranking</p>
-            </a>
+
+        <div class="col-12 col-md-6">
+            <div class="div-enlace">
+                <a href="#">
+                    <img src="/img/iconos/trofeo.png" alt="Enlace 2" class="img-fluid img-perfil">
+                    <h4>Ranking</h4>
+                </a>
+            </div>
         </div>
     </div>
 
+
     <!-- Tercera fila (enlaces, oculta en móvil) -->
-    <div class="row d-none d-md-flex">
-        <!-- Enlace 3 -->
-        <div class="div-enlace col-md-6 link-box">
-            <a href="#">
-                <img src="/img/iconos/editar_perfil.png" alt="Enlace 3" class="img-fluid img-perfil">
-                <p>Editar Perfil</p>
-            </a>
+    <div class="row d-flex justify-content-between mb-4 text-center d-none d-md-flex">
+        <div class="col-12 col-md-6 ">
+            <div class="div-enlace">
+                <a href="#">
+                    <img src="/img/iconos/editar_perfil.png" alt="Enlace 3" class="img-fluid img-perfil">
+                    <h4>Editar perfil</h4>
+                </a>
+            </div>
         </div>
-        <!-- Enlace 4 -->
-        <div class="div-enlace col-md-6 link-box">
-            <a href="#">
-                <img src="/img/iconos/sesion.png" alt="Enlace 4" class="img-fluid img-perfil">
-                <p>Cerrar sesión</p>
-            </a>
+
+        <div class="col-12 col-md-6">
+            <div class="cerrar-sesion">
+                <a href="#">
+                    <img src="/img/iconos/sesion.png" alt="Enlace 4" class="img-fluid img-perfil">
+                    <h4>Cerrar sesión</h4>
+                </a>
+            </div>
         </div>
     </div>
+
 </div>
 @endif
 @endsection
