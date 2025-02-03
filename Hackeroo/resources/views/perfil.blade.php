30df5bb239159638292085e5eabebc264536b931
Lorena <lo.sanchis@gmail.com>
2025-02-03 22:51:14 +0100

    perfil profesor ok

--- Hackeroo/resources/views/perfil.blade.php
+++ Hackeroo/resources/views/perfil.blade.php
@@ -10,8 +10,67 @@
 
 @section('content')
 @if(Auth::user()->rol == 'profesor')
-<h1>img-Perfil del Profesor</h1>
-<p>Aquí va el contenido específico para profesores...</p>
+<div class="container contenedor-perfil d-flex flex-column justify-content-center">
+
+    <div class="perfil row-12 mb-3 d-flex justify-content-center align-items-center ps-5 pe-5">
+        <!-- Imagen y nombre de usuario -->
+        <div class="d-flex flex-row align-items-center mb-3 mb-md-0">
+            <img src="/img/avatares/1.png" alt="Imagen 1" class="img-perfil-cabecera me-3">
+            <h2>Hola, <span id="nombreUsuario">Profesor</span></h2>
+        </div>
+    </div>
+
+    <!-- Segunda fila (enlaces) -->
+    <div class="row d-flex justify-content-between mb-3 text-center ">
+        <div class="col-12 col-md-6 align-items-center">
+            <div class="div-enlace mb-3">
+                <a href="#">
+                    <img src="/img/iconos/cursos.png" alt="Enlace 1" class="img-fluid img-perfil">
+                    <h4>Mis cursos</h4>
+                </a>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-6">
+            <div class="div-enlace">
+                <a href="#">
+                    <img src="/img/iconos/alumnos.png" alt="Enlace 2" class="img-fluid img-perfil">
+                    <h4>Alumnos</h4>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Tercera fila (enlaces, oculta en móvil) -->
+    <div class="row d-flex justify-content-between mb-3 text-center d-none d-md-flex">
+        <div class="col-12 col-md-6">
+            <div class="div-enlace">
+                <a href="#">
+                    <img src="/img/iconos/editar_perfil.png" alt="Enlace 3" class="img-fluid img-perfil">
+                    <h4>Editar perfil</h4>
+                </a>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-6">
+            <div class="cerrar-sesion">
+                <a href="#">
+                    <img src="/img/iconos/sesion.png" alt="Enlace 4" class="img-fluid img-perfil">
+                    <h4>Cerrar sesión</h4>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
 @elseif(Auth::user()->rol == 'alumno')
 <div class="container contenedor-perfil d-flex flex-column justify-content-center">
 
