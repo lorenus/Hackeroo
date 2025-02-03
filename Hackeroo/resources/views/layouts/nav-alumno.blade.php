437a2e45dce5d0ebb8424434d4b931dc3ecaa979
Lorena <lo.sanchis@gmail.com>
2025-02-03 17:22:25 +0100

    comprobacion enlaces OK

--- Hackeroo/resources/views/layouts/nav-alumno.blade.php
+++ Hackeroo/resources/views/layouts/nav-alumno.blade.php
@@ -1,9 +1,9 @@
 <nav>
     <ul>
-        <li><a href="{{ route('dashboard') }}">Inicio</a></li>
-        <li><a href="{{ route('my-courses') }}">Mis Cursos</a></li>
-        <li><a href="{{ route('teachers') }}">Profesores</a></li>
-        <li><a href="{{ route('profile') }}">Perfil</a></li>
-        <li><a href="{{ route('logout') }}">Cerrar Sesión</a></li>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Mis Cursos</a></li>
+        <li><a href="#">Profesores</a></li>
+        <li><a href="#">Perfil</a></li>
+        <li><a href="#">Cerrar Sesión</a></li>
     </ul>
 </nav>
--- Hackeroo/resources/views/layouts/nav-profesor.blade.php
+++ Hackeroo/resources/views/layouts/nav-profesor.blade.php
@@ -1,9 +1,9 @@
 <nav>
     <ul>
-        <li><a href="{{ route('dashboard') }}">Inicio</a></li>
-        <li><a href="{{ route('courses') }}">Mis Cursos</a></li>
-        <li><a href="{{ route('students') }}">Mis Alumnos</a></li>
-        <li><a href="{{ route('profile') }}">Perfil</a></li>
-        <li><a href="{{ route('logout') }}">Cerrar Sesión</a></li>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Mis Cursos</a></li>
+        <li><a href="#">Mis Alumnos</a></li>
+        <li><a href="#">Perfil</a></li>
+        <li><a href="#">Cerrar Sesión</a></li>
     </ul>
 </nav>
