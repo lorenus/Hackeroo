1e5e4d290cda4fcb2c67553948dbc66c2d54b806
Lorena <lo.sanchis@gmail.com>
2025-02-03 17:02:46 +0100

    controler y rutas

--- Hackeroo/resources/views/perfil.blade.php
+++ Hackeroo/resources/views/perfil.blade.php
@@ -1,13 +1,19 @@
 @extends('layouts.app')
 
-@section('content')
+@section('nav')
     @if(Auth::user()->rol == 'profesor')
-        @include('layouts.nav-profesor')
-        <h1>Perfil del Profesor</h1>
-        <p>Aquí va el contenido específico para profesores...</p>
-    @elseif(Auth::user()->role == 'alumno')
-        @include('layouts.nav-Alumno')
-        <h1>Perfil del Alumno</h1>
-        <p>Aquí va el contenido específico para alumnos...</p>
+        @include('layouts.nav-profesor')  
+    @elseif(Auth::user()->rol == 'alumno')
+        @include('layouts.nav-alumno')   
     @endif
+@endsection
+
+@section('content')
+        @if(Auth::user()->rol == 'profesor')
+            <h1>Perfil del Profesor</h1>
+            <p>Aquí va el contenido específico para profesores...</p>
+        @elseif(Auth::user()->rol == 'alumno')
+            <h1>Perfil del Alumno</h1>
+            <p>Aquí va el contenido específico para alumnos...</p>
+        @endif
 @endsection
