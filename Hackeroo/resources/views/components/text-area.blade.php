b5a9a829e5b16407aa9865ec1f3c73243f3b5cb1
Lorena <lo.sanchis@gmail.com>
2025-02-04 18:37:21 +0100

    ajustes componentes text-area e input text para que sean iguales

--- Hackeroo/resources/views/components/text-area.blade.php
+++ Hackeroo/resources/views/components/text-area.blade.php
@@ -8,13 +8,13 @@
     border:none;
     margin-bottom: 15px;
     min-height: 150px; 
-    padding-top: 4px; 
+    padding-top: 2px; 
     font-family: "Itim"; 
     font-size: 18px;
     outline: none;
     box-shadow: none; 
     color: #455A64;
-    line-height: 25px; 
-    background: repeating-linear-gradient(transparent, transparent 23px, #FFB300 23px, #FFB300 25px); 
+    line-height: 26px; 
+    background: repeating-linear-gradient(transparent, transparent 23px, #FFB300 23px, #FFB300 26px); 
     resize: vertical;']) }}
 >{{ $slot }}</textarea>
