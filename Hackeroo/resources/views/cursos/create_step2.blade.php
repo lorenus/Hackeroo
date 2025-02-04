c4fbe3476282f76d2c20265ebba2154c93b4f2e4
Lorena <lo.sanchis@gmail.com>
2025-02-04 21:16:53 +0100

    scroll listado añadir alumnos

--- Hackeroo/resources/views/cursos/create_step2.blade.php
+++ Hackeroo/resources/views/cursos/create_step2.blade.php
@@ -16,31 +16,34 @@
             <h6>Selecciona los alumnos que quieras añadir</h6>
 
             <div class="mb-5 mt-3 text-md-start">
-                <div class="mb-3">
-                    <form action="{{ route('cursos.store.step2') }}" method="POST">
-                        @csrf
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>Nombre</th>
-                                    <th>DNI</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($alumnos as $alumno)
-                                <tr>
-                                    <td>
-                                        <input type="checkbox" name="alumnos[]" value="{{ $alumno->DNI }}">
-                                    </td>
-                                    <td>{{ $alumno->nombre }} {{ $alumno->apellidos }}</td>
-                                    <td>{{ $alumno->DNI }}</td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                </div>
+    <div class="mb-3">
+        <form action="{{ route('cursos.store.step2') }}" method="POST">
+            @csrf
+            <div class="tabla-scroll-container"> <!-- Contenedor para el scroll -->
+                <table> <!-- Eliminamos la clase .tabla-scroll -->
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Nombre</th>
+                            <th>DNI</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($alumnos as $alumno)
+                        <tr>
+                            <td>
+                                <input type="checkbox" name="alumnos[]" value="{{ $alumno->DNI }}">
+                            </td>
+                            <td>{{ $alumno->nombre }} {{ $alumno->apellidos }}</td>
+                            <td>{{ $alumno->DNI }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
+        </form>
+    </div>
+</div>
             <div class="col-12 mt-3 text-center mt-4">
                 <x-primary-button type="submit">Crear curso</x-primary-button>
             </div>
