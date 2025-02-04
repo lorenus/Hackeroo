45a49abccfd76303215bec156ebf859d75bdc2f8
sofia <sofial.garm@gmail.com>
2025-02-04 22:48:27 +0100

    ranking barras y layout

--- Hackeroo/resources/views/ranking/index.blade.php
+++ Hackeroo/resources/views/ranking/index.blade.php
@@ -1,27 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>Ranking de Alumnos</h1>
+<div class="container text-center">
+    <h1>Ranking</h1>
     <p>A continuación, verás a todos los alumnos ordenados por su número de puntos:</p>
+    <div class="contenido container-fluid d-flex flex-column flex-md-row p-3">
+        <!-- Imagen -->
+        <div class="col-12 col-md-6 d-flex justify-content-center text-center order-0 order-md-0">
+            <img src="/img/imagenes/ranking.png" alt="Imagen" class="img-fluid">
+        </div>
 
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Nombre</th>
-                <th>Puntos</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($usuarios as $index => $usuario)
-            <tr>
-                <td>{{ $index + 1 }}</td>
-                <td>{{ $usuario->nombre }} {{ $usuario->apellidos }}</td>
-                <td>{{ $usuario->puntos }} puntos</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+        <!-- Contenedor de ranking y tabla -->
+        <div class="col-12 col-md-6 order-1 order-md-1">
+            <!-- Ranking -->
+            <div class="ranking-container">
+                <!-- Ranking item 1 -->
+                <div class="ranking-item amarillo d-flex justify-content-between h-25 mb-2 p-3">
+                    <h3><span>1</span> <span>Nombre Apellido</span></h3>
+                </div>
+                <!-- Ranking item 2 -->
+                <div class="ranking-item verde d-flex justify-content-between h-25 mb-2 p-3">
+                    <h3><span>2</span> <span>Nombre Apellido</span></h3>
+                </div>
+                <!-- Ranking item 3 -->
+                <div class="ranking-item azul d-flex justify-content-between h-25 mb-2 p-3">
+                    <h3><span>3</span> <span>Nombre Apellido</span></h3>
+                </div>
+            </div>
+
+            <!-- Tabla -->
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Puntos</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($usuarios as $index => $usuario)
+                    <tr>
+                        <td>{{ $index + 1 }}</td>
+                        <td>{{ $usuario->nombre }} {{ $usuario->apellidos }}</td>
+                        <td>{{ $usuario->puntos }} puntos</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 @endsection
