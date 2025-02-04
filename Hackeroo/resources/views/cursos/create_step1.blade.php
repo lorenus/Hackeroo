7a535fc111ee141c90b5c8d78a9d92f533cdccb5
Lorena <lo.sanchis@gmail.com>
2025-02-04 18:26:46 +0100

    base creacion curso (falta adaptar detalles)

--- Hackeroo/resources/views/cursos/create_step1.blade.php
+++ Hackeroo/resources/views/cursos/create_step1.blade.php
@@ -2,30 +2,40 @@
 
 @section('content')
 <div class="container">
-    <h1>Crear Curso - Paso 1</h1>
-
-    <!-- Mostrar mensaje de éxito -->
-    @if(session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
+    <!-- Botón para volver -->
+    <div class="row mb-3 volver">
+        <div class="col-12 text-left">
+            <a href="{{ route('perfil') }}">
+                <img src="/img/botones/volver.png" alt="Volver">
+            </a>
+        </div>
     </div>
-    @endif
+
 
     <form action="{{ route('cursos.store.step1') }}" method="POST">
         @csrf
 
+        <fieldset>
+            <legend class="text-center">Nuevo curso</legend>
+                <x-input-label for="nombre" :value="__('Nombre del curso')" />
+                <x-text-input id="nombre"
+                    class="form-control block"
+                    type="text" name="nombre" :value="old('nombre')" required />
+
+            <!-- 
         <div class="mb-3">
             <label for="nombre" class="form-label">Nombre del Curso</label>
             <input type="text" class="form-control" id="nombre" name="nombre" required>
-        </div>
-
-        <div class="mb-3">
-            <label for="descripcion" class="form-label">Descripción</label>
-            <textarea class="form-control" id="descripcion" name="descripcion" required></textarea>
-        </div>
-
-        <button type="submit" class="btn btn-primary">Continuar</button>
-        <a href="{{ route('cursos') }}" class="btn btn-secondary">Volver</a>
+        </div> -->
+                <x-input-label for="descripcion" :value="__('Descripción')" />
+                <x-text-area class="form-control" id="descripcion" name="descripcion"></x-text-area>
+                <!-- <div class="mb-3">
+                <label for="descripcion" class="form-label">Descripción</label>
+                <textarea class="form-control" id="descripcion" name="descripcion" required></textarea>
+            </div> -->
+            
+            <x-primary-button type="submit" class="btn btn-primary">Continuar</x-primary-button>
+        </fieldset>
     </form>
 </div>
 @endsection
