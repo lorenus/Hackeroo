fa426eb0076522055cfbf1634eea393655c131e9
sofia <sofial.garm@gmail.com>
2025-02-05 08:57:58 +0100

    forgot-password centrado

--- Hackeroo/resources/views/auth/forgot-password.blade.php
+++ Hackeroo/resources/views/auth/forgot-password.blade.php
@@ -9,18 +9,18 @@
     @csrf
 
     <!-- Email Address -->
-    <div>
+    <div class="text-center">
         <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
             {{ __('Indica tu correo y te enviaremos un enlace de recuperación.') }}
         </div>
-        <x-input-label for="email" :value="__('Email')" />
+        <x-input-label for="email" :value="__('Email:')" class="text-center" />
         <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
         <x-input-error :messages="$errors->get('email')" class="mt-2" />
     </div>
 
-    <div class="flex items-center justify-end mt-4">
+    <div class="flex items-center justify-end mt-4 text-center">
         <x-primary-button>
-            {{ __('Email Password Reset Link') }}
+            {{ __('Enviar') }}
         </x-primary-button>
     </div>
 </form>
