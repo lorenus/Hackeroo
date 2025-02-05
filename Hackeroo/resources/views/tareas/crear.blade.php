fe93a5ce9d2a4b5ee39aafc6408a129ea7159277
lorena <lo.sanchis@gmail.com>
2025-02-05 13:09:48 +0100

    form crear tareas solo falta select

--- Hackeroo/resources/views/tareas/crear.blade.php
+++ Hackeroo/resources/views/tareas/crear.blade.php
@@ -2,43 +2,45 @@
 
 @section('content')
 <div class="container">
-    <h1>Crear Nueva Tarea</h1>
+    <h2 class='text-center'>Añadir contenido</h2>
     <form action="{{ route('tarea.guardar') }}" method="POST">
-    @csrf
-    <div class="form-group">
-        <label for="tipo">Tipo de Tarea</label>
-        <select class="form-control" id="tipo" name="tipo" required>
-            <option value="test">Test</option>
-            <option value="archivo">Archivo</option>
-            <option value="link">Link</option>
-        </select>
-    </div>
-    <div class="form-group" id="numero_preguntas_container" style="display: block;">
-        <label for="numero_preguntas">Número de Preguntas</label>
-        <input type="number" class="form-control" id="numero_preguntas" name="numero_preguntas" min="1">
-    </div>
-    <div class="form-group">
-        <label for="titulo">Título</label>
-        <input type="text" class="form-control" id="titulo" name="titulo" required>
-    </div>
-    <div class="form-group">
-        <label for="descripcion">Descripción</label>
-        <textarea class="form-control" id="descripcion" name="descripcion" rows="3" required></textarea>
-    </div>
-    <input type="hidden" name="curso_id" value="{{ $curso_id }}">
-    <button type="submit" class="btn btn-primary">Siguiente</button>
-</form>
+        @csrf
+        <div class="form-group mb-2">
+            <div>
+                <x-input-label for="tipo" value='Tipo de contenido' />
+                <select class="form-control" id="tipo" name="tipo" required>
+                    <option value="test">Test</option>
+                    <option value="archivo">Archivo</option>
+                    <option value="link">Link</option>
+                </select>
+            </div>
+            <div class="form-group mb-2" id="numero_preguntas_container">
+                <label for="numero_preguntas">Número de preguntas</label>
+                <x-number-input class="form-control" id="numero_preguntas" name="numero_preguntas" required />
+            </div>
+        </div>
+        <div class="form-group mb-4">
+            <label for="titulo">Título</label>
+            <x-text-input class="form-control" id="titulo" name="titulo" required />
+        </div>
+        <div class="form-group">
+            <label for="descripcion">Descripción</label>
+            <x-text-area class="form-control" id="descripcion" name="descripcion" rows="3" required />
+        </div>
+        <input type="hidden" name="curso_id" value="{{ $curso_id }}">
+        <x-primary-button type="submit" class="btn btn-primary">Siguiente</x-primary-button>
+    </form>
 </div>
 
 
 <script>
-    document.getElementById('tipo').addEventListener('change', function() {
-        var numeroPreguntasContainer = document.getElementById('numero_preguntas_container');
-        if (this.value === 'test') {
-            numeroPreguntasContainer.style.display = 'block';
-        } else {
-            numeroPreguntasContainer.style.display = 'none';
-        }
-    });
+document.getElementById('tipo').addEventListener('change', function() {
+    var numeroPreguntasContainer = document.getElementById('numero_preguntas_container');
+    if (this.value === 'test') {
+        numeroPreguntasContainer.style.display = 'block';
+    } else {
+        numeroPreguntasContainer.style.display = 'none';
+    }
+});
 </script>
 @endsection
