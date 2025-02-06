ef8ce0afdc4eda62b77cc1404fc4ef93e7868527
sofia <sofial.garm@gmail.com>
2025-02-06 12:02:07 +0100

    vista-preguntas-tarea

--- Hackeroo/resources/views/tareas/ver.blade.php
+++ Hackeroo/resources/views/tareas/ver.blade.php
@@ -10,29 +10,41 @@
         </div>
     </div>
 
-    <div class="row mb-3">
-        <div class="col-12 text-center">
-            <h2>Responde las preguntas de la tarea: {{ $tarea->titulo }}</h2>
-        </div>
-    </div>
-
     <form action="{{ route('tarea.enviar', ['curso_id' => $curso_id, 'tarea_id' => $tarea->id]) }}" method="POST">
         @csrf
+        <fieldset class="tabla-tarea">
+            <legend>
+                    <div class="col-12 text-center">
+                        <h2>{{ $tarea->titulo }}</h2>
+                        <!-- <p>{{ $tarea->descripcion }}</p> -->
+                    </div>
+            </legend>
+            <div class="tabla-scroll-container" id="tabla-scroll-container-tarea">
+                <table class="ver-tareas-alumnos">
+                    @foreach($tarea->preguntas as $pregunta)
+                    <tr>
+                        <td>
+                            <div class="pregunta">
 
-        @foreach($tarea->preguntas as $pregunta)
-        <div class="pregunta">
-            <h5>{{ $pregunta->enunciado }}</h5>
+                                <h5>{{ $pregunta->enunciado }}</h5>
 
-            @foreach($pregunta->opciones_respuestas as $opcion)
-            <div class="form-check">
-                <input type="radio" class="form-check-input" name="pregunta[{{ $pregunta->id }}]" value="{{ $opcion->id }}" id="opcion_{{ $opcion->id }}">
-                <label class="form-check-label" for="opcion_{{ $opcion->id }}">{{ $opcion->respuesta }}</label>
+                                @foreach($pregunta->opciones_respuestas as $opcion)
+                                <div class="align-items-start text-left">
+                                    <input type="radio" class="form-check-input" name="pregunta[{{ $pregunta->id }}]" value="{{ $opcion->id }}" id="opcion_{{ $opcion->id }}">
+                                    <label class="form-check-label" for="opcion_{{ $opcion->id }}">{{ $opcion->respuesta }}</label>
+                                </div>
+
+                                @endforeach
+                            </div>
+                        </td>
+                    </tr>
+                    @endforeach
+                </table>
+                <div class="text-center mt-4">
+                <x-primary-button type="submit" class="btn btn-primary">Enviar respuestas</x-primary-button>
+                </div>
             </div>
-            @endforeach
-        </div>
-        @endforeach
-
-        <button type="submit" class="btn btn-primary">Enviar respuestas</button>
+        </fieldset>
     </form>
 
 </div>
