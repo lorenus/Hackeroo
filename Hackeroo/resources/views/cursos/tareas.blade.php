d78890557db32ac9b2f8dad5fff918cd6512d48b
sofia <sofial.garm@gmail.com>
2025-02-06 09:56:09 +0100

    cuadritos de tareas

--- Hackeroo/resources/views/cursos/tareas.blade.php
+++ Hackeroo/resources/views/cursos/tareas.blade.php
@@ -16,17 +16,30 @@
         </div>
     </div>
 
-    <div class="row">
-        @foreach($curso->tareas as $tarea)
-            <div class="col-md-4">
-                <a href="{{ route('tarea.ver', ['curso_id' => $curso->id, 'tarea_id' => $tarea->id]) }}" class="text-decoration-none text-dark">
-                    <div class="tarea-card">
-                        <h5>{{ $tarea->titulo }}</h5>
-                        <p>{{ $tarea->descripcion }}</p>
+    <div class="row contenido-cursos">
+        <div class="col-12">
+            @if($curso->tareas->isEmpty())
+            <p class="text-center">No tienes tareas aún.</p>
+            @else
+            <div class="contenedor-cursos mw-md-60">
+                <div class="row justify-content-center">
+                    @foreach($curso->tareas as $tarea)
+                    <div class="curso-item col-md-5 ms-md-5 mb-3">
+                        <div class="curso-nombre">
+                            <a href="{{ route('tarea.ver', ['curso_id' => $curso->id, 'tarea_id' => $tarea->id]) }}" class="text-decoration-none text-dark">
+                                <div class="tarea-card">
+                                    <h5>{{ $tarea->titulo }}</h5>
+                                    <p>{{ $tarea->descripcion }}</p>
+                                </div>
+                            </a>
+                            <p>{{ $curso->descripcion }}</p>
+                        </div>
                     </div>
-                </a>
+                    @endforeach
+                </div>
             </div>
-        @endforeach
+            @endif
+        </div>
     </div>
 </div>
 @endsection
