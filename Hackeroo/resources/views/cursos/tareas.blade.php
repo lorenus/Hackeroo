bc4fc3c66e78b2d31bad83dad01ac6932f0929ad
sofia <sofial.garm@gmail.com>
2025-02-07 13:36:25 +0100

    imagenes puestas, falta arregalr estilo y funcionalidad segun tipo de tarea

--- Hackeroo/resources/views/cursos/tareas.blade.php
+++ Hackeroo/resources/views/cursos/tareas.blade.php
@@ -27,7 +27,15 @@
                     <div class="curso-item col-md-5 ms-md-5 mb-3">
                         <div class="curso-nombre text-center">
                             <a href="{{ route('tarea.ver', ['curso_id' => $curso->id, 'tarea_id' => $tarea->id]) }}" class="text-decoration-none text-dark">
-                                <div class="tarea-card">
+                                <div class="tarea-card d-flex flex-column align-items-center">
+                                    <!-- Determinar la imagen según el tipo de tarea -->
+                                    @if ($tarea->tipo === 'test')
+                                    <img src="{{ asset('img/Imagenes/todo.png') }}" alt="Test" class="mb-2">
+                                    @elseif ($tarea->tipo === 'link')
+                                    <img src="{{ asset('img/Imagenes/link.png') }}" alt="Link" class="mb-2">
+                                    @elseif ($tarea->tipo === 'archivo')
+                                    <img src="{{ asset('img/Imagenes/apuntes.png') }}" alt="Archivo" class="mb-2">
+                                    @endif
                                     <h5>{{ $tarea->titulo }}</h5>
                                     <p>{{ $tarea->descripcion }}</p>
                                 </div>
