30785627711bcac7fcd1dc233590d4713352d7a7
sofia <sofial.garm@gmail.com>
2025-02-08 20:59:09 +0100

    tarjetas de alumnos, falta icono de descarga

--- Hackeroo/resources/views/cursos/tareas.blade.php
+++ Hackeroo/resources/views/cursos/tareas.blade.php
@@ -24,20 +24,45 @@
             <div class="contenedor-cursos mw-md-60">
                 <div class="row justify-content-center">
                     @foreach($curso->tareas as $tarea)
-                    <div class="curso-item col-md-5 ms-md-5 mb-3">
+                           <!-- Asignar clase dinámica según el tipo de tarea -->
+                           @php
+                                $borderClass = ''; // Clase CSS para el borde
+                                switch ($tarea->tipo) {
+                                case 'test':
+                                $borderClass = 'border-test'; 
+                                break;
+                                case 'link':
+                                $borderClass = 'border-link'; 
+                                break;
+                                case 'archivo':
+                                $borderClass = 'border-archivo'; 
+                                break;
+                                }
+                                @endphp
+                    <div class="curso-item col-md-5 me-3 mb-3 {{ $borderClass }}">
                         <div class="curso-nombre text-center">
-                            <a href="{{ route('tarea.ver', ['curso_id' => $curso->id, 'tarea_id' => $tarea->id]) }}" class="text-decoration-none text-dark">
-                                <div class="tarea-card d-flex flex-column align-items-center">
-                                    <!-- Determinar la imagen según el tipo de tarea -->
-                                    @if ($tarea->tipo === 'test')
-                                    <img src="{{ asset('img/Imagenes/todo.png') }}" alt="Test" class="mb-2">
-                                    @elseif ($tarea->tipo === 'link')
-                                    <img src="{{ asset('img/Imagenes/link.png') }}" alt="Link" class="mb-2">
-                                    @elseif ($tarea->tipo === 'archivo')
-                                    <img src="{{ asset('img/Imagenes/apuntes.png') }}" alt="Archivo" class="mb-2">
-                                    @endif
-                                    <h5>{{ $tarea->titulo }}</h5>
-                                    <p>{{ $tarea->descripcion }}</p>
+                            <a href="{{ route('tarea.ver', ['curso_id' => $curso->id, 'tarea_id' => $tarea->id]) }}"
+                                class="text-decoration-none text-dark"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                         
+                                <!-- Aplicar la clase dinámica al div -->
+                                <div class="tarea-card d-flex align-items-center p-3">
+                                    <!-- Imagen a un lado -->
+                                    <div class="me-3">
+                                        @if ($tarea->tipo === 'test')
+                                        <img src="{{ asset('img/Imagenes/todo.png') }}" alt="Test" class="img-fluid" style="max-width: 80px;">
+                                        @elseif ($tarea->tipo === 'link')
+                                        <img src="{{ asset('img/Imagenes/link.png') }}" alt="Link" class="img-fluid" style="max-width: 80px;">
+                                        @elseif ($tarea->tipo === 'archivo')
+                                        <img src="{{ asset('img/Imagenes/apuntes.png') }}" alt="Archivo" class="img-fluid" style="max-width: 80px;">
+                                        @endif
+                                    </div>
+                                    <!-- Contenido (título y descripción) -->
+                                    <div class="text-left">
+                                        <h5>{{ $tarea->titulo }}</h5>
+                                        <p class="mb-0">{{ $tarea->descripcion }}</p>
+                                    </div>
                                 </div>
                             </a>
                         </div>
