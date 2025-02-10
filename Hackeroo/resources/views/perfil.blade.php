e16d7f362862ded8cc5347d742a62ab7acfabc06
Lorena <lo.sanchis@gmail.com>
2025-02-10 21:14:16 +0100

    editar perfil todo OK, falta contenedor responsive

--- Hackeroo/resources/views/perfil.blade.php
+++ Hackeroo/resources/views/perfil.blade.php
@@ -11,16 +11,16 @@
 @section('content')
     <div class="container container-perfil d-flex flex-column justify-content-center">
         @if(Auth::user()->rol == 'profesor')
-            <div class="perfil row-12 mb-3 d-flex justify-content-center align-items-center ps-5 pe-5" style="background-color: {{ Auth::user()->color }};">
+            <div class="perfil row-12 mb-3 d-flex justify-content-center align-items-center ps-5 pe-5 cuadrado" style="background-color: {{ Auth::user()->color }};">
                 <div class="d-flex flex-row align-items-center mb-3 mb-md-0">
-                    <img src="/img/avatares/{{ Auth::user()->avatar }}" alt="Imagen 1" class="img-perfil-cabecera me-3">
+                    <img src="/img/avatares/{{ Auth::user()->avatar }}" alt="Imagen 1" class="img-perfil-cabecera me-3 img-fluid">
                     <h2>Hola, <span id="nombreUsuario">{{ Auth::user()->nombre }}</span></h2>
                 </div>
             </div>
 
             <div class="row d-flex justify-content-between mb-3 text-center">
                 <div class="col-12 col-md-6 align-items-center">
-                    <div class="div-enlace mb-3" style="border-color: {{ Auth::user()->color }};">
+                    <div class="div-enlace mb-3 cuadrado" style="border-color: {{ Auth::user()->color }};">
                         <a href="{{ route('cursos') }}">
                             <img src="/img/iconos/cursos.png" alt="Enlace 1" class="img-fluid img-perfil">
                             <h4>Mis cursos</h4>
@@ -29,7 +29,7 @@
                 </div>
 
                 <div class="col-12 col-md-6">
-                    <div class="div-enlace" style="border-color: {{ Auth::user()->color }};">
+                    <div class="div-enlace cuadrado" style="border-color: {{ Auth::user()->color }};">
                         <a href="{{ route('alumnos') }}">
                             <img src="/img/iconos/alumnos.png" alt="Enlace 2" class="img-fluid img-perfil">
                             <h4>Alumnos</h4>
@@ -40,7 +40,7 @@
 
             <div class="row d-flex justify-content-between mb-3 text-center d-none d-md-flex">
                 <div class="col-12 col-md-6">
-                    <div class="div-enlace" style="border-color: {{ Auth::user()->color }};">
+                    <div class="div-enlace cuadrado" style="border-color: {{ Auth::user()->color }};">
                         <a href="{{ route('editar-perfil') }}">
                             <img src="/img/iconos/editar_perfil.png" alt="Enlace 3" class="img-fluid img-perfil">
                             <h4>Editar perfil</h4>
@@ -61,14 +61,14 @@
                 </div>
             </div>
         @elseif(Auth::user()->rol == 'alumno')
-            <div class="perfil row-12 mb-3 d-flex flex-column flex-md-row justify-content-between align-items-center ps-5 pe-5" style="background-color: {{ Auth::user()->color }};">
+            <div class="perfil row-12 mb-3 d-flex flex-column flex-md-row justify-content-between align-items-center ps-5 pe-5 cuadrado" style="background-color: {{ Auth::user()->color }};">
                 <div class="d-flex flex-row align-items-center mb-3 mb-md-0">
-                    <img src="/img/avatares/1.png" alt="Imagen 1" class="img-perfil-cabecera me-3">
+                    <img src="/img/avatares/1.png" alt="Imagen 1" class="img-perfil-cabecera me-3 img-fluid">
                     <h2>Hola, <span id="nombreUsuario">{{ Auth::user()->nombre }}</span></h2>
                 </div>
 
                 <div class="puntuacion d-flex flex-column align-items-center ps-3 pe-3">
-                    <img src="/img/iconos/corona.png" alt="Imagen 2" class="img-corona d-none d-md-block">
+                    <img src="/img/iconos/corona.png" alt="Imagen 2" class="img-corona d-none d-md-block img-fluid">
                     <div class="d-flex flex-row flex-md-column">
                         <h5 class="text-center">Nivel 3</h5>
                         <h5>{{ Auth::user()->puntos }} puntos</h5>
@@ -78,7 +78,7 @@
 
             <div class="row d-flex justify-content-between mb-3 text-center">
                 <div class="col-12 col-md-6">
-                    <div class="div-enlace mb-3" style="border-color: {{ Auth::user()->color }};">
+                    <div class="div-enlace mb-3 cuadrado" style="border-color: {{ Auth::user()->color }};">
                         <a href="{{route('cursos-alumno')}}">
                             <img src="/img/iconos/cursos.png" alt="Enlace 1" class="img-fluid img-perfil">
                             <h4>Mis cursos</h4>
@@ -87,7 +87,7 @@
                 </div>
 
                 <div class="col-12 col-md-6">
-                    <div class="div-enlace" style="border-color: {{ Auth::user()->color }};">
+                    <div class="div-enlace cuadrado" style="border-color: {{ Auth::user()->color }};">
                         <a href="{{ route('ranking') }}">
                             <img src="/img/iconos/trofeo.png" alt="Enlace 2" class="img-fluid img-perfil">
                             <h4>Ranking</h4>
@@ -98,7 +98,7 @@
 
             <div class="row d-flex justify-content-between mb-3 text-center d-none d-md-flex">
                 <div class="col-12 col-md-6">
-                    <div class="div-enlace" style="border-color: {{ Auth::user()->color }};">
+                    <div class="div-enlace cuadrado" style="border-color: {{ Auth::user()->color }};">
                         <a href="{{ route('editar-perfil') }}">
                             <img src="/img/iconos/editar_perfil.png" alt="Enlace 3" class="img-fluid img-perfil">
                             <h4>Editar perfil</h4>
--- Hackeroo/resources/views/profile/edit.blade.php
+++ Hackeroo/resources/views/profile/edit.blade.php
@@ -1,28 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-
 @php
+$colores = ['#FFD87F' => 0,'#455A64' => 0,'#EF5350' => 0,'#FFB300' => 0,'#06AAF4' => 0,'#8CC34C' => 0,'#00FF00' => 10,'#0000FF' => 20, '#FFFF00' => 40,'#FF00FF' => 80,'#00FFFF' => 160,'#808080' => 320];
 
-$colores = ['#FFD87F' => 0,'#455A64' => 0,'#EF5350' => 0,'#FFB300' => 0,'#06AAF4' => 0,'#8CC34C' => 0,'#00FF00' =>
-10,'#0000FF' => 20,
-'#FFFF00' => 40,'#FF00FF' => 80,'#00FFFF' => 160,'#808080' => 320];
-
-$avatares = ['1.png' => 0,'2.png' => 0,'3.png' => 0,'4.png' => 0,'5.png' => 0,'6.png' => 0,'7.png' => 10,'8.png' => 20,
-'9.png' => 40,'10.png' => 80,'11.png' => 160,'12.png' => 320];
-
+$avatares = ['1.png' => 0,'2.png' => 0,'3.png' => 0,'4.png' => 0,'5.png' => 0,'6.png' => 0,'7.png' => 10,'8.png' => 20, '9.png' => 40,'10.png' => 80,'11.png' => 160,'12.png' => 320];
 @endphp
 
-<div class="container">
+<div class="container mb-5 pb-5">
     <div class="row mb-3 volver">
-        <div class="col-12 text-left">
+        <div class="col-6 col-md-12 text-left">
             <a href="{{ route('perfil') }}">
                 <img src="/img/botones/volver.png" alt="Volver">
             </a>
         </div>
     </div>
 
-    <h2 class='text-center mb-3'>Editar Perfil</h2>
+    <div class="col-6 col-md-12 text-left">
+        <h2 class='text-center mb-3'>Editar Perfil</h2>
+    </div>
 
     @if (session('status'))
     <div class="alert alert-success">{{ session('status') }}</div>
@@ -34,100 +30,93 @@ $avatares = ['1.png' => 0,'2.png' => 0,'3.png' => 0,'4.png' => 0,'5.png' => 0,'6
         <input type="hidden" name="color" id="color" value="">
         <input type="hidden" name="avatar" id="avatar" value="">
 
-
-        <div class="row justify-content-center gx-5">
-            <div class="col-md-5 d-flex justify-content-center">
-                <div class='editar-color mb-5'>
-                    <h5>Color del perfil</h5>
-                    <div class='row row-cols-4 g-1'>
-                    
-                        @foreach ($colores as $color => $valor)
-                        <div class="col">
-                            @if ($valor <= Auth::user()->puntos)
-                               
-                                <div class="color-box hand-cursor" data-color="{{ $color }}"
-                                    style="background-color: {{ $color }}; width: 100px; height: 100px; border-radius: 20px;">
-                                </div>
-                                @else
-                                <div
-                                    style="background-image: url('/img/iconos/candado.png'); background-position: center center; background-size: contain;width: 100px; height: 100px; border-radius: 20px;">
-                                </div>
-                                @endif
-                        </div>
-                        @endforeach
+        <div class="row">
+            <!-- Sección de colores -->
+            <div class="col-md-6 col-12 mb-3">
+                <h5 class="text-center mb-3">Color del perfil</h5>
+                <div class="row row-cols-4 g-2">
+                    @foreach ($colores as $color => $valor)
+                    <div class="col">
+                        @if ($valor <= Auth::user()->puntos)
+                            <div class="color-box hand-cursor p-3 text-center ratio ratio-1x1" data-color="{{ $color }}" style="background-color: {{ $color }}; border-radius: 20px; max-width: 100px; max-height: 100px;">
+                            </div>
+                            @else
+                            <div class="p-3 text-center ratio ratio-1x1" style="background-image: url('/img/iconos/candado.png'); background-position: center center; background-size: contain; border-radius: 20px; max-width: 100px; max-height: 100px;">
+                            </div>
+                            @endif
                     </div>
-                </div>
-            </div>
-            <div class="col-md-5 d-flex justify-content-center">
-                <div class='editar-avatar mb-5'>
-                    <h5>Imagen perfil</h5>
-                    <div class='row row-cols-4 g-1'>
-                        @foreach ($avatares as $avatar => $valor)
-                        <div class="col">
-                            @if ($valor <= Auth::user()->puntos)
-                                <div class="avatar-box hand-cursor" data-avatar="{{ $avatar }}"
-                                    style="background-image: url('/img/avatares/{{ $avatar }}'); background-position: center center; background-size: contain;width: 100px; height: 100px; border-radius: 20px;">
-                                </div>
-                                @else
-                                <div
-                                    style="background-image: url('/img/iconos/candado.png'); background-position: center center; background-size: contain;width: 100px; height: 100px; border-radius: 20px;">
-                                </div>
-                                @endif
-                        </div>
-                        @endforeach
-                    </div>
+                    @endforeach
                 </div>
             </div>
 
+            <!-- Sección de avatares -->
+            <div class="col-md-6 col-12">
+                <h5 class="text-center mb-3">Imagen perfil</h5>
+                <div class="row row-cols-4 g-2">
+                    @foreach ($avatares as $avatar => $valor)
+                    <div class="col">
+                        @if ($valor <= Auth::user()->puntos)
+                            <div class="avatar-box hand-cursor p-3 text-center ratio ratio-1x1" data-avatar="{{ $avatar }}" style="background-image: url('/img/avatares/{{ $avatar }}'); background-size: cover; background-position: center; border-radius: 20px; max-width: 100px; max-height: 100px;">
+                            </div>
+                            @else
+                            <div class="p-3 text-center ratio ratio-1x1" style="background-image: url('/img/iconos/candado.png'); background-position: center center; background-size: contain; border-radius: 20px; max-width: 100px; max-height: 100px;">
+                            </div>
+                            @endif
+                    </div>
+                    @endforeach
+                </div>
+            </div>
         </div>
 
-</div>
+        <div class="d-flex justify-content-center align-items-center mt-5">
+            <x-primary-button type="submit" class="btn btn-primary">Actualizar</x-primary-button>
+        </div>
+    </form>
 </div>
 
-<div class="d-flex justify-content-center align-items-center">
-    <x-primary-button type="submit" class="btn btn-primary mt-5 text-center">Actualizar</x-primary-button>
-</div>
-</form>
-</div>
 <script>
     document.addEventListener('DOMContentLoaded', function() {
+        console.log("Script cargado correctamente."); // Depuración
+
+        // Seleccionar los cuadros de color
         const colorBoxes = document.querySelectorAll('.color-box');
-        console.log('Número de cuadros de color:', colorBoxes.length);
+        console.log("Cuadros de color encontrados:", colorBoxes.length); // Depuración
 
         colorBoxes.forEach(function(colorBox) {
             colorBox.addEventListener('click', function() {
+                console.log("Cuadro de color clickeado:", colorBox); // Depuración
                 const colorSeleccionado = colorBox.getAttribute('data-color');
-                console.log('Color seleccionado:', colorSeleccionado);
-
                 document.getElementById('color').value = colorSeleccionado;
 
-
+                // Quitar el borde de todos los cuadros de color
                 colorBoxes.forEach(function(box) {
                     box.style.border = '';
                 });
 
+                // Añadir borde al cuadro seleccionado
                 colorBox.style.border = '3px solid #455A64';
-                console.log('Borde actualizado para el cuadro seleccionado');
+                console.log("Borde añadido al cuadro seleccionado:", colorBox); // Depuración
             });
         });
 
+        // Seleccionar los cuadros de avatar
         const avatarBoxes = document.querySelectorAll('.avatar-box');
-        console.log('Número de avatares:', avatarBoxes.length);
+        console.log("Cuadros de avatar encontrados:", avatarBoxes.length); // Depuración
 
         avatarBoxes.forEach(function(avatarBox) {
             avatarBox.addEventListener('click', function() {
+                console.log("Cuadro de avatar clickeado:", avatarBox); // Depuración
                 const avatarSeleccionado = avatarBox.getAttribute('data-avatar');
-                console.log('Avatar seleccionado:', avatarSeleccionado);
-
                 document.getElementById('avatar').value = avatarSeleccionado;
 
-
+                // Quitar el borde de todos los cuadros de avatar
                 avatarBoxes.forEach(function(box) {
                     box.style.border = '';
                 });
 
+                // Añadir borde al cuadro seleccionado
                 avatarBox.style.border = '3px solid #455A64';
-                console.log('Borde actualizado para el cuadro seleccionado');
+                console.log("Borde añadido al cuadro seleccionado:", avatarBox); // Depuración
             });
         });
     });
