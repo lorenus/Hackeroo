742cdd0e6e8ff594d7d4c97e491510a7c860b096
lorena <lo.sanchis@gmail.com>
2025-02-11 12:44:47 +0100

    vista tareas curso profesor OK

--- Hackeroo/resources/views/cursos/show.blade.php
+++ Hackeroo/resources/views/cursos/show.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class="container">
- <!-- Botón para volver -->
+    <!-- Botón para volver -->
     <div class="row mb-3 mt-5">
         <div class="col-12 text-left">
             <a href="{{ route('cursos') }}">
@@ -23,7 +23,7 @@
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     </div>
-@endif
+    @endif
 
     <h2 class='text-center mb-3'>{{ $curso->nombre }} - Tareas</h2>
 
@@ -40,21 +40,48 @@
             <div class='contenedor-cursos mw-md-60'>
                 <div class="row justify-content-center">
 
-                    @foreach ($curso->tareas as $tarea)
+                    <!-- Asignar clase dinámica según el tipo de tarea -->
+                    
 
-                    <div class="curso-item col-md-5 me-3 mb-3">
+                    @foreach ($curso->tareas as $tarea)
+                    @php
+                    $borderClass = '';
+                    switch ($tarea->tipo) {
+                    case 'test':
+                    $borderClass = 'border-test';
+                    break;
+                    case 'link':
+                    $borderClass = 'border-link';
+                    break;
+                    case 'archivo':
+                    $borderClass = 'border-archivo';
+                    break;
+                    }
+                    @endphp
+                    <div class="curso-item col-md-5 me-3 mb-3 {{ $borderClass }}">
 
                         <!-- Parte izquierda: Nombre del curso -->
-                        <div class="curso-nombre">
-
-                            <a href="{{ route('tarea.ver', ['curso_id' => $curso->id, 'tarea_id' => $tarea->id]) }}">
-                                <h6>{{ $tarea->titulo }}</h6>
+                        <div class="curso-nombre pe-1">
+                            @if($tarea->tipo == 'link')
+                            <a href="{{ route('tarea.ver', ['curso_id' => $curso->id, 'tarea_id' => $tarea->id]) }} "
+                                target="_blank">
+                                <h6 class='hand-cursor'>{{ $tarea->titulo }}</h6>
                             </a>
+                            @elseif($tarea->tipo == 'archivo')
+                            <a href="{{ asset('storage/' . $tarea->recursoMultimedia->url) }}" download target="_blank">
+                                <h6 class='hand-cursor'>{{ $tarea->titulo }}</h6>
+                            </a>
+                            @else
+                            <a href="{{ route('tarea.ver', ['curso_id' => $curso->id, 'tarea_id' => $tarea->id]) }} ">
+                                <h6 class='hand-cursor'>{{ $tarea->titulo }}</h6>
+                            </a>
+                            @endif
+
                             <p>{{$tarea->descripcion}}</p>
                         </div>
 
                         <!-- Parte derecha: Acciones (editar y eliminar) -->
-                        <div class="curso-acciones d-flex flex-column gap-2">
+                        <div class="curso-acciones d-flex flex-column gap-2 ">
                             <!-- Enlace de editar -->
                             <div class="accion-editar">
                                 <!-- <a href="{{ route('cursos.edit', $curso->id) }}" class="btn btn-sm "> -->
