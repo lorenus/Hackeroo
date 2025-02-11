ae8076b1e48643660708213889a99678ac17c8c0
lorena <lo.sanchis@gmail.com>
2025-02-11 11:49:29 +0100

    vista editar tarea OK

--- Hackeroo/resources/views/tareas/edit-recurso.blade.php
+++ Hackeroo/resources/views/tareas/edit-recurso.blade.php
@@ -2,7 +2,22 @@
 
 @section('content')
 <div class="container">
-    <h2>Editar tarea</h2>
+    <!-- Botón para volver -->
+    <div class="row mb-3 mt-5">
+        <div class="col-12 text-left">
+            <a href="{{ route('perfil') }}">
+                <img src="/img/botones/volver.png" alt="Volver">
+            </a>
+        </div>
+    </div>
+
+     <!-- Título centrado -->
+     <div class="row mb-3">
+        <div class="col-12 text-center">
+            <h2>Editar tarea</h2>
+        </div>
+    </div>
+    
 
     <!-- Agregar una clase para ocultar elementos -->
     <style>
@@ -18,7 +33,7 @@
         <!-- Tipo -->
         <div class="mb-3">
             <label for="tipo" class="form-label">Tipo de recurso:</label>
-            <select name="tipo" id="tipo" class="form-select" required>
+            <select name="tipo" id="tipo" class="form-select custom-select" required>
                 <option value="archivo" {{ $tarea->recursoMultimedia->tipo === 'archivo' ? 'selected' : '' }}>Archivo</option>
                 <option value="link" {{ $tarea->recursoMultimedia->tipo === 'link' ? 'selected' : '' }}>Enlace</option>
             </select>
@@ -27,7 +42,7 @@
         <!-- Campo para archivo -->
         <div id="archivo-container" class="mb-3 {{ $tarea->recursoMultimedia->tipo === 'archivo' ? '' : 'hidden' }}">
             <label for="archivo" class="form-label">Archivo:</label>
-            <input type="file" name="archivo" id="archivo" class="form-control">
+            <x-file-input name="archivo" id="archivo" class="form-control"/>
             @if ($tarea->recursoMultimedia->tipo === 'archivo')
                 <p class="text-muted mt-2">Archivo actual: <a href="{{ Storage::url($tarea->recursoMultimedia->url) }}" target="_blank">{{ basename($tarea->recursoMultimedia->url) }}</a></p>
             @endif
@@ -36,11 +51,14 @@
         <!-- Campo para enlace -->
         <div id="link-container" class="mb-3 {{ $tarea->recursoMultimedia->tipo === 'link' ? '' : 'hidden' }}">
             <label for="url" class="form-label">URL:</label>
-            <input type="text" name="url" id="url" class="form-control" value="{{ $tarea->recursoMultimedia->tipo === 'link' ? $tarea->recursoMultimedia->url : '' }}">
+            <x-text-input name="url" id="url" class="form-control" value="{{ $tarea->recursoMultimedia->tipo === 'link' ? $tarea->recursoMultimedia->url : '' }}"/>
         </div>
 
-        <button type="submit" class="btn btn-primary">Guardar cambios</button>
     </form>
+    <div class="col-12 mt-3 text-center mt-4">
+            <x-primary-button type="submit" class="btn btn-primary">Guardar</x-primary-button>
+        </div>
+
 </div>
 
 <script>
