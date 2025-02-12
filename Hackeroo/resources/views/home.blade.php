d4139ae638ffca82329281f5a84d344069ed64b1
Lorena <lo.sanchis@gmail.com>
2025-02-12 17:54:11 +0100

    correccion errata en footer

--- Hackeroo/resources/views/home.blade.php
+++ Hackeroo/resources/views/home.blade.php
@@ -118,7 +118,7 @@
 
             <!-- Segundo div (centro) -->
             <div class="col-12 col-md-4 text-center order-3 order-md-2">
-                <p>&copy; 2025 Mi Página. Todos los derechos reservados.</p>
+                <p>&copy; 2025 Hackeroo. Todos los derechos reservados.</p>
             </div>
 
             <!-- Tercer div (derecha) -->
